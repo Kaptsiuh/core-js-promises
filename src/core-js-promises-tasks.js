e840dc5cc75bb2a072c98fec06edd568ea2079ac
Dima Berry <dimaberry1@gmail.com>
2024-04-15 16:42:31 +0200

    feat: solve takes an array of promises and returns a promise that resolves with value of the first successfuly response

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -58,8 +58,16 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    promises.forEach((p) => {
+      p.then(resolve).catch(() => {
+        if (!promises.every()) {
+          reject(new Error('Promises rejected'));
+        }
+      });
+    });
+  });
 }
 
 /**
